0d06b905f9b45151fd4ea7e6ee95f0cb9d32ab27
Tomer Gozlan <tomgoz96@gmail.com>
2022-11-22 23:20:20 +0200

    open files

---
+++ C_Ex2/my_mat.c
@@ -0,0 +1,5 @@
+//
+// Created by Tomer Gozlan on 22/11/2022.
+//
+
+#include "my_mat.h"
---
+++ C_Ex2/my_mat.h
@@ -0,0 +1,8 @@
+//
+// Created by Tomer Gozlan on 22/11/2022.
+//
+
+#ifndef C_EX2_MY_MAT_H
+#define C_EX2_MY_MAT_H
+
+#endif //C_EX2_MY_MAT_H
